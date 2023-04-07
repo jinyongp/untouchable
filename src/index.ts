a22d9ae8ecc31f9453d6acdb73dbd098d636744a
jinyongp <dev.jinyongp@gmail.com>
2023-04-07 21:14:46 +0900

    Migrate typescript and add build script

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+type Touchable = () => void;
+
+function untouchable<
+  From extends Record<PropertyKey, any>,
+  Key extends keyof From,
+  Args extends Parameters<From[Key]>
+>(
+  from: From,
+  key: Key,
+  handler: (this: From, ...args: Args) => void
+): Touchable {
+  const _ = Proxy.revocable(from[key], {
+    apply(target, thisArg: From, args: Args) {
+      Reflect.apply(handler, thisArg, args);
+      return Reflect.apply(target, thisArg, args);
+    },
+  });
+
+  from[key] = _.proxy.bind(from);
+
+  const prototype = Object.getPrototypeOf(from[key]);
+  prototype.toString = new Proxy(prototype.toString, {
+    apply: (_, thisArg) => `function ${thisArg.name}() { [native code] }`,
+  });
+
+  return _.revoke;
+}
+
+export default untouchable;
