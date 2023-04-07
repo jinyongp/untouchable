cb5992e12443fb0fee4fb7d107f803e81aaa5ccd
jinyongp <dev.jinyongp@gmail.com>
2023-04-08 02:39:53 +0900

    v0.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,19 @@
-type Revoke = () => void;
-
 function untouchable<
   From extends Record<PropertyKey, any>,
   Key extends keyof From,
   Args extends Parameters<From[Key]>
->(from: From, key: Key, handler: (this: From, ...args: Args) => void): Revoke {
+>(from: From, key: Key, handler: (this: From, ...args: Args) => void) {
   const _ = Proxy.revocable(from[key], {
     apply(target, thisArg: From, args: Args) {
       Reflect.apply(handler, thisArg, args);
       return Reflect.apply(target, thisArg, args);
     },
   });
-  from[key] = _.proxy.bind(from);
   const prototype = Object.getPrototypeOf(from[key]);
   prototype.toString = new Proxy(prototype.toString, {
-    apply: (_, thisArg) => `function ${thisArg.name}() { [native code] }`,
+    apply: (__, thisArg) => `function ${thisArg.name}() { [native code] }`,
   });
-
+  from[key] = _.proxy;
   return _.revoke;
 }
 
