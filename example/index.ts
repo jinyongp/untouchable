81bb361b1829933504cbef2392204142e54e9a47
jinyongp <dev.jinyongp@gmail.com>
2023-04-30 14:01:01 +0900

    Add example code

---
+++ example/index.ts
@@ -0,0 +1,27 @@
+import untouchable from '../src';
+
+class Example {
+  multiplier = 3;
+
+  func(a: number, b: number) {
+    return (a + b) * this.multiplier;
+  }
+}
+
+function Patcher(this: Example, a: number, b: number) {
+  console.log('Patched successfully!');
+  console.assert(this instanceof Example);
+  console.assert(this.multiplier === 3);
+  console.assert(a === 1);
+  console.assert(b === 2);
+}
+
+const revoke = untouchable(Example.prototype, 'func', Patcher);
+
+const example = new Example();
+
+console.assert(example.func(1, 2) === 9); // Patched
+
+revoke();
+
+console.assert(example.func(3, 4) === 21); // Not patched
