47e37d0b7253ca6c458b096fdc28a520d68f157f
jinyongp <dev.jinyongp@gmail.com>
2025-11-18 14:42:24 +0900

    move esbuild to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: [
+    'src/index.ts',
+  ],
+
+  tsconfig: "./tsconfig.json",
+
+  dts: {
+    build: true,
+  },
+
+  minify: true,
+  shims: true,
+  clean: true,
+
+  format: ['esm', 'cjs'],
+})
