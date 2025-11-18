d54805b8012e9c16b93f3cda2e6457d66dc24e3f
jinyongp <dev.jinyongp@gmail.com>
2025-11-18 18:46:34 +0900

    add eslint

---
+++ eslint.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'eslint/config'
+
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
+import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  tseslint.configs.recommended,
+  stylistic.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+])
